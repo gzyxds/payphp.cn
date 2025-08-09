3927d7b439e8b9c18de81f6279ebb4b6fb31b2c7
艺创AIGC <348310633@qq.com>
2025-08-10 05:05:46 +0800

    11111

--- components/Docs/SidebarLink.tsx
+++ components/Docs/SidebarLink.tsx
@@ -109,7 +109,7 @@ export default function SidebarLink() {
 
   // 获取扁平化图标
   const getIcon = (iconName: string | undefined) => {
-    const iconMap: { [key: string]: React.JSX.Element } = {
+    const iconMap: { [key: string]: JSX.Element } = {
       '📖': (
         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
           <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"/>
